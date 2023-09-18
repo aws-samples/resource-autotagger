fc180ca972b31fdb4bb7dd7ab23a57f4976d3eab
Ubuntu <ubuntu@ip-172-31-9-1.ca-central-1.compute.internal>
2023-09-18 21:22:17 +0000

    Initial commit

---
+++ test/resource-auto-tag-cdk.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const ResourceAutoTagCdk = require('../lib/resource-auto-tag-cdk-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/resource-auto-tag-cdk-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ResourceAutoTagCdk.ResourceAutoTagCdkStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
